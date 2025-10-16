4663faf9f6f515cad19fe288fd025dea321ca908
tugrr <75561645+tugrr@users.noreply.github.com>
2025-10-17 01:11:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -163,4 +163,8 @@ def notify_owner(client_number, client_name):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
